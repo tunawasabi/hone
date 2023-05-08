2e9f98e1113c124677df1af3eb2a0c4ebf3f7d81
Wasabi <77270077+tunawasabi@users.noreply.github.com>
2023-05-08 14:10:07 +0900

    clear LogSender when the server exited (#33)
    * clear LogSender when the server exited
    
    * refactoring

--- src/executor/mcsv.rs
+++ src/executor/mcsv.rs
@@ -15,7 +15,7 @@ impl StdinSender {
         thread::spawn(move || {
             for v in receiver {
                 // write_allでコマンドを実行させるために最後に改行を加える
-                if let Err(_) = self.stdin.write_all(format!("{}\n", v).as_bytes()) {
+                if self.stdin.write_all(format!("{}\n", v).as_bytes()).is_err() {
                     break;
                 };
             }
